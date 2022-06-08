f01e54d51a161931188570bfe4e33f775008e8e2
TaroFrappucino <slamets.marcel13@gmail.com>
2022-06-08 23:18:21 +0700

    Project Pong - Bola mantul

---
+++ Assets/BallController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    // Fungsi yang otomatis dijalankan pada saat Game Object diaktifkan pertama kali, baik langsung saat game dijalankan, saat scene diload, atau diaktifkan manual dengan menggunakan script 
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>(); 
+        rig.velocity = speed;
+    }
+
+    // Update is called once per frame
+    // Fungsi yang otomatis dijalankan berulang-ulang selama Game Object aktif setelah Start dijalankan. Jika game object selalu aktif, maka fungsi ini akan berjalan terus menerus selama game berjalan. Lebih tepatnya fungsi ini dijalankan tiap Frame.
+    void Update()
+    {
+        // Mengubah posisi objek
+        // Vector3 (sb x, sb y, sb z)
+        // Deltatime untuk mengatur perubahan posisi bola menggunakan per 1 detik dengan cara mengalikannya dengan waktu yang dibutuhkan untuk menjalankan 1 frame
+        // Transform.position = transform.position + (new Vector3(0.1f, 0, 0) * Time.deltaTime);
+
+        // fungsi Translate untuk memindah objek secara langsung daripada menggunakan transform.position
+        // transform.Translate(speed * Time.deltaTime);
+    }
+   
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rig;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Object
+        moveObject(getInput());
+    }
+
+    // Terima input
+    private Vector2 getInput() {
+        if (Input.GetKey(upKey)) {
+            return Vector2.up * speed;
+        }
+        if (Input.GetKey(downKey)) {
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+
+    private void moveObject(Vector2 movement) {
+        rig.velocity = movement;
+        // transform.Translate(movement * Time.deltaTime);
+    }
+}
